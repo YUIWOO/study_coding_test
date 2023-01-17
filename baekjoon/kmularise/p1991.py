1e4963507b4dddb181a3f7b557286ec8e3437486
Yuijin Kim <yuikim@c5r5s7.42seoul.kr>
2023-01-17 10:21:51 +0900

    solve

--- baekjoon/kmularise/p1991.py
+++ baekjoon/kmularise/p1991.py
@@ -8,20 +8,21 @@ class BinaryTree():
 	def __init__(self):
 		self.root = None
 	def find(self, n, item):
-		temp = None
-		print("check", n.item)
-		print(n.item == item)
+		temp_right = None
+		temp_left = None
 		if n != None:
 			if n.item == item:
 				return n
-			else:
-				if n.left:
-					return self.find(n.left, item)
-				if n.right:
-					return self.find(n.right, item)
+			if n.left:
+				temp_left = self.find(n.left, item)
+			if n.right:
+				temp_right = self.find(n.right, item)
+		if temp_left != None:
+			return temp_left
+		return temp_right
 	def preorder(self, n):
 		if n != None:
-			print(n.item, '', end='')
+			print(n.item, end='')
 			if n.left:
 				self.preorder(n.left)
 			if n.right:
@@ -32,12 +33,12 @@ class BinaryTree():
 				self.postorder(n.left)
 			if n.right:
 				self.postorder(n.right)
-			print(n.item,'',end = '')
+			print(n.item,end = '')
 	def midorder(self, n):
 		if n != None:
 			if n.left:
 				self.midorder(n.left)
-			print(n.item, '', end='')
+			print(n.item, end='')
 			if n.right:
 				self.midorder(n.right)
 
@@ -46,24 +47,25 @@ n = int(input())
 tree = BinaryTree()
 #tree.root = Node("A")
 
-parent1, left1, right1 = map(Node, input().split())
+parent1, left1, right1 = map(str, input().split())
 
-tree.root = parent1
-parent1.left = left1
-parent1.right = right1
+tree.root = Node(parent1)
+if left1 != ".":
+	tree.root.left = Node(left1)
+if right1 != ".":
+	tree.root.right = Node(right1)
 
 for i in range(1, n):
 	parent, left, right = map(str,input().split())
 	parent_node = tree.find(tree.root, parent)
-	print(parent_node.item)
 	if left != ".":
 		parent_node.left = Node(left)
 	if right != ".":
 		parent_node.right = Node(right)
 
-print(tree.root.item)
 tree.preorder(tree.root)
 print()
 tree.midorder(tree.root)
 print()
 tree.postorder(tree.root)
+print()

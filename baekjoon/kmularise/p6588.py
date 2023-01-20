5f8ebb22cee270e27b6eb0b3906202f67df58672
Yuijin Kim <yuikim@c5r9s6.42seoul.kr>
2023-01-20 20:11:49 +0900

    solve p6588.py

--- baekjoon/kmularise/p6588.py
+++ baekjoon/kmularise/p6588.py
@@ -5,6 +5,11 @@ def check_goldbach(num, sieve):
 			return True
 	print("Goldbach's conjecture is wrong.")
 	return False
+#i => sieve[i]
+# i 가 소수면 sieve[i] true
+# i 가 소수가 아니면 sieve[i] false
+# 42  41까지만 소수인지 판별
+
 
 def prime_sieve(n):
 	sieve = [True] * n
@@ -13,18 +18,19 @@ def prime_sieve(n):
 	sieve[1] = False
 	for i in range(2, max_sqrt_n + 1):
 		if sieve[i] == True:
-			for j in range(2 * i, n, i):
+			for j in range(i * i, n, i):
 				sieve[j] = False
-	sieve[2] = False
+	# sieve[2] = False
 	return sieve
-
+sieve = prime_sieve(1000000)
 num_set = []
 while True:
 
 	num = int(input())
 	if num == 0:
 		break
-	num_set.append(num)
-sieve = prime_sieve(max(num_set))
-for num in num_set:	
-	check_goldbach(num,sieve)
+	check_goldbach(num, sieve)
+	# num_set.append(num)
+# sieve = prime_sieve(max(num_set))
+# for num in num_set:	
+# 	check_goldbach(num,sieve)
